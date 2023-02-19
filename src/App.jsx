e4f5c258621b08fbada796146ea2f9fb81c78842
shokouhSkr <shokouhskr@gmail.com>
2023-02-19 21:01:16 +0330

    tailwindcss and mui added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="bg-red-500 m-1 p-8 flex md:bg-fuchsia-100 items-center justify-center h-screen">
+      <Button variant="contained">hi to you dear</Button>
+    </div>
+  );
+}
+
+export default App;
