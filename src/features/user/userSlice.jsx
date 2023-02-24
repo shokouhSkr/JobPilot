a8d9ed9a1d67292b8e1763d886d19cfa35c5355b
shokouhSkr <shokouhskr@gmail.com>
2023-02-24 23:32:43 +0330

    login user with http requset

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -12,13 +12,17 @@ export const registerUser = createAsyncThunk("user/registerUser", async (user, t
     const res = await customFetch.post("/auth/register", user);
     return res.data;
   } catch (error) {
-    // console.log(error.response.data.msg);
     return thunkAPI.rejectWithValue(error.response.data.msg);
   }
 });
 
 export const loginUser = createAsyncThunk("user/loginUser", async (user, thunkAPI) => {
-  console.log("Login user: ", user);
+  try {
+    const res = await customFetch.post("/auth/login", user);
+    return res.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
 });
 
 const userSlice = createSlice({
@@ -39,6 +43,22 @@ const userSlice = createSlice({
         toast.success(`Hello There, ${user.name}`);
       })
       .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        toast.error(action.payload);
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        const { user } = action.payload;
+
+        state.isLoading = false;
+        state.user = user;
+        console.log(action.payload.user);
+
+        toast.success(`Welcome Back, ${user.name}`);
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         toast.error(action.payload);
       });
