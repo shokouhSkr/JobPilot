b2b29af69557ea3457781c4fa4202cfe4c3d6f4c
shokouhSkr <shokouhskr@gmail.com>
2023-02-27 00:11:25 +0330

    dashboard header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,51 @@
+import React, { useState } from "react";
+import { GrMenu } from "react-icons/gr";
+import { Button, Menu, MenuItem, Fade, Avatar } from "@mui/material";
+
 const Header = () => {
-  return <header className="h-20 bg-red-300 text-right md:ml-80">header</header>;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <header className="flex h-20 items-center justify-end px-1 pt-4 text-2xl text-main md:ml-80">
+      <p className="w-full px-3 md:text-center">Dashboard</p>
+      <span className="py-1.5 px-2 md:hidden">
+        <GrMenu />
+      </span>
+      <div>
+        <Button
+          id="fade-button"
+          aria-controls={open ? "fade-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <Avatar src="/broken-image.jpg" className="h-8 w-8 bg-primary" />
+        </Button>
+        <Menu
+          id="fade-menu"
+          MenuListProps={{
+            "aria-labelledby": "fade-button",
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
