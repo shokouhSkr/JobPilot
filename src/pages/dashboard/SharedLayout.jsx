3f23db71d3e08346f0ddab3c6388a231c6919b6c
shokouhSkr <shokouhskr@gmail.com>
2023-02-26 16:44:03 +0330

    sharedLayout added

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { Header, Sidebar } from "../../components";
+import { Outlet } from "react-router-dom";
+
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <main className="grid h-screen grid-cols-12 grid-rows-[80px_minmax(500px,_1fr)] bg-screen font-roboto text-screen">
+      <Header />
+      <Sidebar />
+      <Outlet />
+    </main>
+  );
 };
 
 export default SharedLayout;
