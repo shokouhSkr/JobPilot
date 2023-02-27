94b3612aabc39c9cdcdddac3d952686dc6e41762
shokouhSkr <shokouhskr@gmail.com>
2023-02-28 01:28:19 +0330

    backdrop added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,46 +11,55 @@ const Sidebar = () => {
   const dispatch = useDispatch();
 
   return (
-    <aside
-      className={`${
-        isSidebarOpen ? "translate-x-0" : "-translate-x-80"
-      } fixed left-0 top-0 bottom-0 z-50 m-4 w-72 rounded-xl bg-main transition-all duration-300 md:translate-x-0`}
-    >
-      <button
-        type="button"
+    <>
+      {/* backdrop */}
+      <div
+        className={`${isSidebarOpen && "fixed top-0 left-0 z-40 h-screen w-full md:hidden"}`}
         onClick={() => dispatch(closeSidebar())}
-        className="absolute top-3 right-3 text-lg text-screen md:hidden"
+      ></div>
+
+      {/* overlay */}
+      <aside
+        className={`${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-80"
+        } fixed left-0 top-0 bottom-0 z-50 m-4 w-72 rounded-xl bg-main transition-all duration-300 md:translate-x-0`}
       >
-        <VscClose />
-      </button>
+        <button
+          type="button"
+          onClick={() => dispatch(closeSidebar())}
+          className="absolute top-3 right-3 text-lg text-screen md:hidden"
+        >
+          <VscClose />
+        </button>
 
-      <div className="p-5">
-        <Logo sidebar />
-      </div>
+        <div className="p-5">
+          <Logo sidebar />
+        </div>
 
-      <hr className="mb-8" />
+        <hr className="mb-8" />
 
-      <ul className="px-4">
-        {Links.map((link) => {
-          const { id, title, path, icon } = link;
-          return (
-            <li key={id}>
-              <NavLink
-                to={path}
-                className={({ isActive }) =>
-                  `mb-1 flex items-center justify-start gap-4 rounded-lg px-10 py-4 text-lg transition-all duration-[400ms] ${
-                    isActive ? "bg-primary pl-12" : "hover:bg-hover hover:pl-12"
-                  }`
-                }
-              >
-                <span>{icon}</span>
-                <span>{title}</span>
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </aside>
+        <ul className="px-4">
+          {Links.map((link) => {
+            const { id, title, path, icon } = link;
+            return (
+              <li key={id}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) =>
+                    `mb-1 flex items-center justify-start gap-4 rounded-lg px-10 py-4 text-lg transition-all duration-[400ms] ${
+                      isActive ? "bg-primary pl-12" : "hover:bg-hover hover:pl-12"
+                    }`
+                  }
+                >
+                  <span>{icon}</span>
+                  <span>{title}</span>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </aside>
+    </>
   );
 };
 
